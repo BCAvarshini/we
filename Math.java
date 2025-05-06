b6af71632e0cfe7d659773b2b83dbd3a15b99de8
BCAvarshini <varshinijakkali@gmail.com>
2025-05-06 09:55:19 +0530

    description

---
+++ Math.java
@@ -0,0 +1,16 @@
+
+public class Math {
+
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+    }
+
+}
